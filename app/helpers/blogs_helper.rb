106c019603b82e06fd63d0626ad2f2a785cb1bbe
lef237 <lef237k5@gmail.com>
2022-12-02 21:05:53 +0900

    sanitizeメソッドを使ってscriptタグの埋め込みをできなくした

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,6 +2,8 @@
 
 module BlogsHelper
   def format_content(blog)
+    # ここでhtml.safeが使われているのが原因らしい。
+    # こちらをそのままの状態で、viewのほうにsanitizeメソッドを使ってみる。
     blog.content.gsub("\n", '<br>').html_safe # rubocop:disable Rails/OutputSafety
   end
 end
