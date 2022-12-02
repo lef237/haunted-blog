3843960b6cb41a4836d9a638ec3a790b3042b877
lef237 <lef237k5@gmail.com>
2022-12-02 22:43:31 +0900

    他の人の秘密のブログにアクセスできないようにした

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -11,6 +11,11 @@ class BlogsController < ApplicationController
 
   def show
     @blog = Blog.find(params[:id])
+    if @blog.secret && @blog.user != current_user
+      redirect_to blogs_url, notice: 'This page cannot be accessed.'
+    else
+      @blog = Blog.find(params[:id])
+    end
   end
 
   def new
