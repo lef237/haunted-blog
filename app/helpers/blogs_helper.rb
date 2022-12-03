437f4c14cdff89d1cb8c8d13002a5ec3e85b5b21
lef237 <lef237k5@gmail.com>
2022-12-03 17:58:55 +0900

    テスト全部通るようにした

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,10 +2,7 @@
 
 module BlogsHelper
   def format_content(blog)
-    # ここでエスケープした後、\nをgsubでbrタグだけを作成する（タグの文字自体は残る）
     content = html_escape(blog.content).gsub("\n", '<br>')
-    # そのままではrailsの機能で<br>もエスケープされてしまうので、<br>だけ許可する
-    # content.html_safeでも問題ないが、念のためsanitizeで処理する。
-    sanitize(content, tags: %w(br))
+    sanitize(content)
   end
 end
